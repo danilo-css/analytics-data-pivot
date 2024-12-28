9ea4a825d1375492f2b8f34f80a19ad7f4ba9bbd
danilo-css <73763541+danilo-css@users.noreply.github.com>
2024-12-28 14:03:16 -0300

    Update InitWasm.tsx

--- components/InitWasm.tsx
+++ components/InitWasm.tsx
@@ -2,6 +2,7 @@
 
 import { useDuckDBStore } from "@/stores/useDuckDBStore";
 import { usePyodideStore } from "@/stores/usePyodideStore";
+import { CircleCheck, CircleX, Loader2 } from "lucide-react";
 import React, { useEffect } from "react";
 
 export default function InitWasm() {
@@ -16,13 +17,41 @@ export default function InitWasm() {
 
   return (
     <>
-      {loadingduckdb && <p>Loading DuckDB...</p>}
-      {db && <p>DuckDB initialized succesfully.</p>}
-      {errorduckdb && <p>Error initializing DuckDB: {errorduckdb.message}</p>}
-      {loadingpyodide && <p>Loading Pyodide...</p>}
-      {pyodide && <p>Pyodide initialized succesfully.</p>}
+      {loadingduckdb && (
+        <div className="flex flex-row items-center">
+          <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+          <p>Loading DuckDB...</p>
+        </div>
+      )}
+      {db && (
+        <div className="flex flex-row items-center">
+          <CircleCheck color="#008a35" className="mr-2 h-4 w-4" />
+          <p>DuckDB initialized succesfully.</p>
+        </div>
+      )}
+      {errorduckdb && (
+        <div className="flex flex-row items-center">
+          <CircleX color="#ff0000" className="mr-2 h-4 w-4" />
+          <p>Error initializing DuckDB: {errorduckdb.message}</p>
+        </div>
+      )}
+      {loadingpyodide && (
+        <div className="flex flex-row items-center">
+          <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+          <p>Loading Pyodide...</p>
+        </div>
+      )}
+      {pyodide && (
+        <div className="flex flex-row items-center">
+          <CircleCheck color="#008a35" className="mr-2 h-4 w-4" />
+          <p>Pyodide initialized succesfully.</p>
+        </div>
+      )}
       {errorpyodide && (
-        <p>Error initializing Pyodide: {errorpyodide.message}</p>
+        <div className="flex flex-row items-center">
+          <CircleX color="#ff0000" className="mr-2 h-4 w-4" />
+          <p>Error initializing Pyodide: {errorpyodide.message}</p>
+        </div>
       )}
     </>
   );
