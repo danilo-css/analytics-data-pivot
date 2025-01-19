8fccb18982138dcf84496e8cbb3cb2901ac56a8d
danilo-css <73763541+danilo-css@users.noreply.github.com>
2025-01-19 10:47:38 -0300

    Styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Inter as FontSans } from "next/font/google";
+import { Geist as FontSans } from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
 import { cn } from "@/lib/utils";
